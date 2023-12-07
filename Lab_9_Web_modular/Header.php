f18502689f7694bab19516806ba9b3ade065bd7b
deanpratamasaputraa <pratamadean257@gmail.com>
2023-12-07 22:54:46 +0700

    File Baruu

---
+++ Lab_9_Web_modular/Header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+    <header>
+<h1>Modularisasi Menggunakan Require</h1>
+</header>
+<nav>
+<a href="home.php">Home</a>
+<a href="about.php">Tentang</a>
+<a href="kontak.php">Kontak</a>
+</nav>  
+    </div>
+</body>
+</html>
---
+++ lab9_php_praktikum/footer.php
@@ -0,0 +1,6 @@
+<footer>
+            <p>Created by <a href="">Dean Pratama</a> | &copy; 2023</p>
+        </footer>
+    </div>
+</body>
+</html>
